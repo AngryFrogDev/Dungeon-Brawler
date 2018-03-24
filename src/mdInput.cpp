d6bf9f59c56c7d44be987ada1af5ea117a41a936
Sergi PR <t3m1X@users.noreply.github.com>
2018-03-24 23:03:20 +0100

    Fixing errors caused during merge of mdCollisions branch

--- src/mdInput.cpp
+++ src/mdInput.cpp
@@ -277,11 +277,6 @@ void mdInput::handleAxes(const SDL_Event & event) {
 	}
 }
 
-KEY_STATE mdInput::getKey(int key) 	{
-
-	return keyboard[key];
-}
-
 
 KEY_STATE mdInput::getKey(SDL_Scancode key) const{
 	return keyboard[key];
