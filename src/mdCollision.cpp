20f8c9f03a61b36eb79708aadb6909e0029f95b4
Skyway666 <lucasgarciamateu@gmail.com>
2018-03-25 12:56:11 +0200

    Fixed collisions crashing

--- src/mdCollision.cpp
+++ src/mdCollision.cpp
@@ -93,10 +93,11 @@ bool mdCollision::cleanUp()
 
 	for (std::list<collider*>::iterator it = colliders.begin(); it != colliders.end(); ++it) {
 		collider* c = *it;
-		colliders.remove(c);
 		delete c;
 
 	}
+
+	colliders.clear();
 
 	return true;
 }
