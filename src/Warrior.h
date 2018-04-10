c5ad783aac6c516ac07a2e2ce204d5d23c1866ff
Skyway666 <lucasgarciamateu@gmail.com>
2018-04-10 23:02:25 +0200

    Commented some code

--- src/Warrior.h
+++ src/Warrior.h
@@ -21,7 +21,7 @@ public:
 	void updateAnimationOnBasicAttack(CHAR_ATT_TYPE type); // Just to use for super
 
 private:
-
+	// -To load from xml-
 	int spin_speed;
 
 	int swordyuken_invencivility; // In milliseconds
@@ -37,7 +37,9 @@ private:
 	iPoint jm_s2_speed;
 
 	item dive_kick_object, spin_object, swap_object; // Swap object is not being used
+	// -To load from xml-
 
+	// I would leave it hardcoded for now
 	std::list<CHAR_ATT_TYPE> super_attack_list;
 	CHAR_ATT_TYPE super_last_attack;
 	std::list<CHAR_ATT_TYPE>::iterator super_iterator;
